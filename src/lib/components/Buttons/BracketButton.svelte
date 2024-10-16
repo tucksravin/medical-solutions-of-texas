02014e5e19582f99718e708bf94d7dacd9d5c802
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-10-16 15:21:48 -0700

    more changes, outline

--- src/lib/components/Buttons/BracketButton.svelte
+++ src/lib/components/Buttons/BracketButton.svelte
@@ -19,7 +19,7 @@
     {href}
 >
  <div class=" transition-transform duration-300 text-xl {isActive ? "-translate-x-1":""}">[</div>
-    <div class=" mx-1 uppercase no-underline translate-y-[2px]"><slot/></div>
+    <div class=" mx-1 uppercase no-underline translate-y-[2px] button-text"><slot/></div>
     <div class="  transition-transform duration-300 text-xl {isActive ? "translate-x-1":""}">]</div>
 </a>
 {:else}
@@ -30,7 +30,7 @@
     class="flex flex-row items-center text-center no-underline justify-center transition-all duration-300 active:-translate-y-2 {$$props.class || ''}" 
 >
 <div class=" transition-transform duration-300 text-xl {isActive ? "-translate-x-2":""}">[</div>
-    <div class="mx-2 uppercase no-underline translate-y-[2px]"><slot/></div>
+    <div class="mx-2 uppercase no-underline translate-y-[2px] button-text"><slot/></div>
     <div class=" transition-transform duration-300 text-xl {isActive ? "translate-x-2":""}">]</div>
 </button>
 {/if}
--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -3,9 +3,15 @@
 import usFlag from "$lib/assets/icons/logos/usFlag.svg"
 import txFlag from "$lib/assets/icons/logos/txFlag.svg"
 import sdvosbLogo from "$lib/assets/icons/logos/sdvosb-white.svg";
+import revogenLogo from "$lib/assets/icons/logos/revogenLogo.png" 
 
 //images
 import soldiersVideoPlaceholder from "$lib/assets/images/home/bgVideoPlaceholder.jpg"
+import surgeonsVideoPlaceholder from "$lib/assets/images/home/surgeonsPlaceholder.png"
+import timeline1 from "$lib/assets/images/home/timeLine.svg"
+import timeline2 from "$lib/assets/images/home/timeline2.svg"
+import timeline3 from "$lib/assets/images/home/timeline3.svg"
+import ctaImage from "$lib/assets/images/home/doctor.png"
 
   
 //components
@@ -21,6 +27,7 @@ import soldiersVideoPlaceholder from "$lib/assets/images/home/bgVideoPlaceholder
 
 
 let viewportWidth:number;
+let viewportHeight:number;
 
 let isMounted = false;
 let showSubtitle = false;
@@ -28,6 +35,35 @@ let showSubtitle = false;
 let middleTitleLine:HTMLElement |  null;
 
 let middleTitleLineWidth = writable(3000);
+
+let weGetIndex = 0;
+
+let weGetTrigger:HTMLElement | null;
+
+const runWeGetAnimation = () => {
+	if(weGetIndex!=2){
+		weGetIndex=1;
+		setTimeout(()=>weGetIndex=2,1600)
+	}
+}
+
+const runReverseWeGetAnimation = () => {
+
+	if(weGetIndex!=0){
+		weGetIndex=1;
+		setTimeout(()=>weGetIndex=0,1600)
+	}
+}
+
+let painTrigger:HTMLElement | null;
+let isPainActive = false;
+
+let threeStepTrigger:HTMLElement | null;
+let isthreeStepActive = false;
+
+let ctaTrigger:HTMLElement | null;
+let isCtaActive = false;
+
 
 const handleResize = () => {
   if (middleTitleLine) {
@@ -38,6 +74,49 @@ const handleResize = () => {
     console.log("Middle title line element not found");
   }
 }
+
+const handleScroll = () => {
+	if(weGetTrigger){
+		const weGetBottom = weGetTrigger.getBoundingClientRect().bottom
+
+		if(weGetBottom<viewportHeight){
+			runWeGetAnimation()
+		}else{
+			runReverseWeGetAnimation()
+		}
+	}
+
+	if(painTrigger){
+		const painBottom =painTrigger.getBoundingClientRect().bottom
+
+		if(painBottom<viewportHeight){
+			isPainActive=true;
+		}else{
+			isPainActive=false;
+		}
+	}
+
+	if(threeStepTrigger){
+		const threeStepBottom =threeStepTrigger.getBoundingClientRect().bottom
+
+		if(threeStepBottom<viewportHeight){
+			isthreeStepActive=true;
+		}else{
+			isthreeStepActive=false;
+		}
+	}
+
+
+	if(ctaTrigger){
+		const ctaBottom =ctaTrigger.getBoundingClientRect().bottom
+
+		if(ctaBottom<viewportHeight){
+			isCtaActive=true;
+		}else{
+			isCtaActive=false;
+		}
+	}
+}
 		
 onMount(() => {
 
@@ -47,6 +126,7 @@ onMount(() => {
 	isMounted = true;
 
     window.addEventListener('resize', handleResize);
+	window.addEventListener('scroll', handleScroll);
 
 	
 	setTimeout(()=>{
@@ -70,7 +150,7 @@ onMount(() => {
 
 </script>
 
-<svelte:window bind:innerWidth={viewportWidth} />
+<svelte:window bind:innerWidth={viewportWidth} bind:innerHeight={viewportHeight}/>
 
 <ScreenWidthImage class="fixed -z-10" src={soldiersVideoPlaceholder} vimeoId="1019997263" darken backdrop>
 	
@@ -80,9 +160,9 @@ onMount(() => {
 		{#if isMounted}
 		<div class="w-full h-full flex flex-col justify-end py-20 relative"  transition:fade={{duration:700}}>
 			{#if viewportWidth>768 }
-				<h1 class="text-mid transition-opacity duration-700 ease-fast-slow delay-700 {isMounted ? "opacity-50":"opacity-0"}">Connecting DoD + VA</h1>
-				<h1 bind:this={middleTitleLine} class="text-mid  w-fit transition-opacity duration-700 ease-fast-slow delay-700 {isMounted ? "opacity-50":"opacity-0"}">Hospitals with</h1>
-				<h1 class="text-mid  transition-opacity duration-700 ease-fast-slow delay-700 {isMounted ? "opacity-50":"opacity-0"}"> Med-Surg Suppliers</h1>
+				<h1 class="text-mid transition-opacity duration-1000 ease-out  delay-700 {isMounted ? "opacity-50":"opacity-0"}">Connecting DoD + VA</h1>
+				<h1 bind:this={middleTitleLine} class="text-mid  w-fit transition-opacity duration-1000 ease-out  delay-700 {isMounted ? "opacity-50":"opacity-0"}">Hospitals with</h1>
+				<h1 class="text-mid  transition-opacity duration-1000 ease-outdelay-700 {isMounted ? "opacity-50":"opacity-0"}"> Med-Surg Suppliers</h1>
 				{#if showSubtitle}
 				<div transition:fade={{duration:700}} class="text-white text-nowrap
 						md:absolute md:w-[20%] md:bottom-[204px] md:text-[10px] md:leading-tight 
@@ -96,7 +176,7 @@ onMount(() => {
 				
 			{:else}
 				<h1 class="text-mid  mb-12 opacity-50">Connecting <br/> DoD + VA <br/> Hospitals <br/> w/ Med-Surg <br/> Suppliers</h1>
-				<div class="text-white transition-opacity duration-700 ease-fast-slow {showSubtitle? "opacity-100":"opacity-0"}">We bridge the gap between clinical demand and government procurement to support America’s Armed Services and our Veterans.</div>
+				<div class="text-white transition-opacity duration-1000 ease-out  {showSubtitle? "opacity-100":"opacity-0"}">We bridge the gap between clinical demand and government procurement to support America’s Armed Services and our Veterans.</div>
 			{/if}
 		</div>
 		{/if}
@@ -122,3 +202,129 @@ onMount(() => {
 	</div>
 
 </ContentWidth>
+
+<section class="w-full py-20 bg-mid" >
+	<ContentWidth class="items-start">
+		<div class="w-full aspect-[5/2] bg-light relative overflow-hidden">
+			<img src={surgeonsVideoPlaceholder} alt="surgeons placeholder" class="w-full h-full absolute top-0 left-0 object-cover"/>
+			<iframe 
+					title="background video" 
+					src={`https://player.vimeo.com/video/1019997302?background=1`}
+					class="absolute w-full aspect-video top-0 left-0 contrast-[1.15]"
+					frameborder="0"
+					
+				  ></iframe>
+				  <h2 class="text-mid absolute -bottom-[14px] left-0 leading-none">We'll Get You</h2>
+		</div>
+		<div class="mt-6 overflow-hidden" >
+		<div class="mt-6 overflow-hidden" bind:this={weGetTrigger}>
+			<div class="transition-transform duration-1000 ease-out" style={`transform:translateY(calc(-1/3 * 100% * ${weGetIndex}))`}>
+				<h2 class="text-dark text-left transition duration-1000 ease-out {weGetIndex===0 ? "":"opacity-25"}">Priority Access</h2>
+				<h2 class="text-dark text-left transition duration-1000 ease-out {weGetIndex===1 ? "":"opacity-25"}">contract expertise</h2>
+				<h2 class="text-dark text-left transition duration-1000 ease-out {weGetIndex===2 ? "":"opacity-25"}">Direct Results</h2>
+			</div>
+		
+			<h5 class="text-dark mt-36 mb-5">The End User In Mind</h5>
+			<div class="text-dark w-80">MSOT makes medical care possible from the war fighter all the way to the retiree and all phases of service inbetween.</div>
+	</ContentWidth>
+</section>
+<section class="bg-light w-screen aspect-[5/2] relative">
+	<img src={timeline1} alt="timelines" class="w-full" />
+</section>
+<section class="bg-mid py-20" bind:this={painTrigger}>
+	<ContentWidth class="flex flex-col md:flex-row relative">
+		{#if !isPainActive}
+			<div class="absolute top-0 left-0 h-[2px] bg-gradient-to-r transition-all from-dark to-transparent  w-full" transition:fade={{duration:6000}} />
+		{/if}
+	
+		{#if isPainActive}
+			<div class="absolute top-0 left-0 h-[2px] bg-gradient-to-r transition-all from-transparent to-dark  w-full" transition:fade={{duration:6000}} />
+		{/if}
+		<div class="md:w-1/2 mt-6 transition-opacity duration-1000 ease-out {isPainActive?"":"opacity-0"}">
+			<div class="duration-1000 ease-out {isPainActive?"opacity-25 delay-[2800ms]":""}">
+				<h4 class="text-dark mb-20 duration-1000 ease-out {isPainActive?"opacity-100":""}">We Know Your Pain </h4>
+				<h3 class="text-dark duration-1000 ease-out  {isPainActive?"opacity-100":"opacity-0"}">government <br/> contracts are</h3>
+				<h3 class="text-dark duration-1000 ease-out  {isPainActive?"opacity-100 delay-700":"opacity-0"}">opaque</h3>
+				<h3 class="text-dark duration-1000 ease-out  {isPainActive?"opacity-100 delay-[1400ms]":"opacity-0"}">confusing</h3>
+				<h3 class="text-dark duration-1000 ease-out  {isPainActive?"opacity-100 delay-[2100ms]":"opacity-0"}">inaccessible</h3>
+			</div>
+		</div>
+		<div class="md:w-1/2 mt-6">
+			<h4 class="text-dark mb-20  duration-1000 ease-out {isPainActive?"opacity-100 delay-[3500ms]":"opacity-0"}">We Know What To Do</h4>
+			<h3 class="text-dark duration-1000 ease-out  {isPainActive?"opacity-100 delay-[3500ms]":"opacity-0"}">with us <br/> You’ll get</h3>
+			<h3 class="text-dark duration-1000 ease-out {isPainActive?"opacity-100 delay-[4200ms]":"opacity-0"}">transparency</h3>
+			<h3 class="text-dark duration-1000 ease-out {isPainActive?"opacity-100 delay-[4900ms] ":"opacity-0"}">understanding</h3>
+			<h3 class="text-dark duration-1000 ease-out {isPainActive?"opacity-100  delay-[5600ms]  ":"opacity-0"}">access</h3>
+		</div>
+		
+	</ContentWidth>
+</section>
+<section class="bg-mid flex flex-col items-center justify-center gap-10 py-32">
+	<h4 class="text-dark">We’re here to help</h4>
+	<p class="text-dark text-center">Take the headache and confusion out of dealing <br /> with government healthcare </p>
+</section>
+
+<section class="bg-light w-screen aspect-[5/2] relative">
+	<img src={timeline2} alt="timelines" class="w-full" />
+</section>
+<section class="bg-mid py-32">
+	<ContentWidth>
+	<div class="w-full flex justify-center items-center">
+		<div class="w-full py-32 sm:py-32 lg:px-40 border-[#998B6A] border-b-[6px] border-x-[6px] relative flex flex-col items-center justify-center gap-10 mb-12">
+			<div class="absolute top-0 left-0 w-6 sm:w-1/6 md:w-1/4 lg:w-1/3 h-0 border-[#998B6A] border-t-[6px]" />
+			<div class="absolute top-0 right-0 w-6 sm:w-1/6 md:w-1/4 lg:w-1/3 h-0 border-[#998B6A] border-t-[6px]" />
+			<h4 class="text-dark absolute top-0 left-1/2 text-nowrap" style="transform:translate(-50%, calc(-50% + 3px))">Our Partners</h4>
+
+				
+				<img src={revogenLogo} alt="service disabled veteran owned small business logo" class="h-32"/>
+				
+			
+			<p class="text-dark text-center text-[24px]">MSOT made the process of getting our product into the DoD system easier than we ever thought possible. They're an invaluable partnership and wealth of knowledge when it comes to selling to the DoD and VA hospital systems.</p>
+			
+		</div>
+	</div>
+
+</ContentWidth>
+</section>
+<section class="bg-mid py-20" bind:this={threeStepTrigger}>
+	<ContentWidth>
+		<h2 class="text-dark text-left w-full">Getting started</h2>
+		<h2 class="text-dark text-left w-full">is simple with msot</h2>
+		<div class="w-full my-24 flex flex-col md:flex-row justify-between gap-8">
+			<div class="w-full relative transition-opacity duration-1000 ease-out {isthreeStepActive?"bg-opacity-100":"opacity-0"}">
+				<div class="absolute top-0 left-0 h-[2px] bg-gradient-to-r transition-all from-dark to-transparent  w-full" />
+				<h4 class="text-dark mt-4">01</h4>
+				<h3 class="text-dark mt-8">establish <br/>	goals</h3>
+			</div>
+			<div class="w-full relative transition-opacity duration-1000 ease-out {isthreeStepActive?"bg-opacity-100 delay-[800ms]":"opacity-0"}">
+				<div class="absolute top-0 left-0 h-[2px] bg-gradient-to-r transition-all from-dark to-transparent  w-full" />
+				<h4 class="text-dark mt-4">02</h4>
+				<h3 class="text-dark mt-8">Find Your <br/> access points</h3>
+			</div>
+			<div class="w-full relative transition-opacity duration-1000 ease-out {isthreeStepActive?"bg-opacity-100 delay-[1800ms]":"opacity-0"}">
+				<div class="absolute top-0 left-0 h-[2px] bg-gradient-to-r transition-all from-dark to-transparent  w-full" />
+				<h4 class="text-dark mt-4">03</h4>
+				<h3 class="text-dark mt-8">Set pricing <br/>direct invoicing</h3>
+
+			</div>
+		</div>
+		<BracketButton class="text-dark mb-12" href="/about">Learn More</BracketButton>
+	</ContentWidth>
+</section>
+<section class="bg-light w-screen aspect-[5/2] relative">
+	<img src={timeline3} alt="timelines" class="w-full" />
+	
+</section>
+<ScreenWidthImage src={ctaImage} class="flex flex-col items-start justify-end py-32" darken>
+	<div class="h-44 overflow-hidden relative w-full">
+	
+			<h1 class=" text-mid add-outline absolute bottom-0 left-0 opacity-40">let’s get started</h1>
+			<h1 class="outlined absolute bottom-0 left-0 transition-opacity duration-1000 {isCtaActive ? "opacity-100  delay-700" : "opacity-0"}">let’s get started</h1>
+			<h1 class=" text-mid add-outline absolute bottom-0 left-0 transition-opacity duration-1000   {isCtaActive ? "opacity-100 delay-[1400ms]" : "opacity-0"}">let’s get started</h1>
+		
+	</div>
+	<div class="flex flex-row gap-6 ml-2" bind:this={ctaTrigger}>
+		<BracketButton class="text-mid" href="/">Contact Us</BracketButton>
+		<BracketButton class="text-mid" href="/">Learn More</BracketButton>
+	</div>
+</ScreenWidthImage>
