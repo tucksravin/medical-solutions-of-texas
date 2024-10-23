87ba25e9eddf5932aa78e95a1f4d77ad102ab3b5
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-10-23 12:33:34 -0700

    d

--- src/lib/components/ScreenWidth/ScreenWidthImage.svelte
+++ src/lib/components/ScreenWidth/ScreenWidthImage.svelte
@@ -32,10 +32,11 @@
 				
 					<iframe 
 					title="background video" 
-					src={`https://player.vimeo.com/video/${vimeoId}?background=1`}
+					src={`https://player.vimeo.com/video/${vimeoId}?background=1&muted=1&loop=1&autoplay=1`}
 					class="aspect-video absolute {viewportHeight * 16 > viewportWidth * 9 ? 'h-screen min-w-full' : 'w-screen min-h-full'} contrast-[1.15] -z-10"
 					frameborder="0"
 					allowfullscreen
+					
 					
 				  ></iframe>
 			{/if}
