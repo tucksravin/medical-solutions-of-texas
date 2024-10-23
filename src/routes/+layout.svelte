35f61c11f6d27d919cdcc8ebe753dbea9466cd1a
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-10-23 11:02:30 -0700

    sandy logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { repositoryName } from '$lib/prismicio';
 	import { fly, fade, scale } from 'svelte/transition';
 	import msotLogo from "$lib/assets/icons/logos/msot_logo.svg"
+	import msotLogoSand from "$lib/assets/icons/logos/msot_logo_sand.svg"
 	import sdvosbLogo from "$lib/assets/icons/logos/sdvosb-white.svg"
 	import "../app.css";
   import ContentWidth from '$lib/components/ContentWidth/ContentWidth.svelte';
@@ -61,17 +62,17 @@
 	}
 
 
-  let isMounted = false;
-  let isTransitioning = true;
+  let isReady = false;
+  let isTransitioning = false;
 
 
   onMount(()=>{
-	isMounted = true;
+	
 
 	window.addEventListener('scroll', handleScroll)
 
 	setTimeout(()=>{
-		isTransitioning = false;
+		isReady = true;
 	}, 2400)
   })
 
@@ -103,14 +104,15 @@
 
 <!-- <button class="bg-black p-2 bump text-white z-50 fixed top-12 right-12" on:click={()=>isTransitioning=!isTransitioning}> {isTransitioning?"transitioning":" not transitioning"}</button> -->
 
-{#if isTransitioning}
-	<div class="bg-[#140F09] z-40 fixed w-screen h-screen top-0 left-0" out:fade={{duration:700, delay:350}} ></div>
+{#if isTransitioning||!isReady}
+	<div class="bg-[#140F09] z-40 fixed w-screen h-screen top-0 left-0" out:fade={{duration:700, delay:700}} ></div>
 {/if}
 
+{#if isReady}
 	{#if viewportWidth<1550}
 		<OnMount>
 			<div 
-				in:fade={{duration:700}}
+				in:fade={{duration:700, delay:700}}
 				class="fixed z-40 top-4 left-0 w-40 transition-transform transform-gpu duration-700 {isTransitioning?"delay-100 pointer-events-none":""}" 
 				style={isTransitioning?"transform:translate( calc(50vw - 50%), calc(50vh - 50% - 16px)) scale(200%)":"transform:translate(4vw)"}
 			>		
@@ -120,7 +122,7 @@
 	{:else}
 		<OnMount>
 			<div 
-				in:fade={{duration:700}}
+				in:fade={{duration:700, delay:700}}
 				class="fixed z-40 top-4 left-0 w-40 transition-transform transform-gpu duration-700 {isTransitioning?"delay-100 pointer-events-none":""}" 
 				style={isTransitioning?"transform:translate( calc(50vw - 50%), calc(50vh - 50% - 16px)) scale(200%)":"transform:translate(calc( (100vw - 1440px) / 2 ))"}
 			>		
@@ -128,6 +130,11 @@
 			</div>
 		</OnMount>
 	{/if}
+{:else}
+	<div transition:fade class="fixed w-2/5 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-40">
+		<img src={msotLogoSand} class="w-full h-full pulse-always" alt="msot logo"/>
+	</div>
+{/if}
 	
 	
 {#if isOverlayVisible}
