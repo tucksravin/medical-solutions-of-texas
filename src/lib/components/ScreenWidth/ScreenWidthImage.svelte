8a8ed6ecc12a89abe6536b992590f32745bfd35f
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-10-23 12:39:47 -0700

    autoplay

--- src/lib/components/ScreenWidth/ScreenWidthImage.svelte
+++ src/lib/components/ScreenWidth/ScreenWidthImage.svelte
@@ -23,7 +23,7 @@
 	
 	<svelte:window bind:innerHeight={viewportHeight} bind:innerWidth={viewportWidth} />
 	
-	<section class="h-screen w-screen relative overflow-clip {backdrop?"fixed h-screen w-screen -z-10 top-0 left-0":""}">
+	<section class="h-screen w-screen overflow-clip {backdrop?"fixed -z-10 top-0 left-0":"relative"}">
 		<div class="right-0 left-0 overflow-clip max-h-screen aspect-video relative {viewportHeight * 16 > viewportWidth * 9 ? 'h-screen min-w-full' : 'w-screen min-h-full'}">
 			
 				<img src={src} alt={altText} class="absolute bottom-0 {placeholderSide}-0 h-full w-full object-cover {src===placeholder ? "lg:w-[45%] md:h-auto" : ""} -z-10"/>
@@ -44,7 +44,7 @@
 			{#if darken}
 			<div class="bg-darken-gradient pointer-events-none absolute w-full h-full top-0 left-0 -z-10" />
 			{/if}
-			<div class="w-screen h-full">
+			<div class="w-screen h-screen absolute top-0 left-0">
 			<ContentWidth class='{$$props.class || 'flex items-center justify-center'} h-full'>
 				<slot />
 			</ContentWidth>
