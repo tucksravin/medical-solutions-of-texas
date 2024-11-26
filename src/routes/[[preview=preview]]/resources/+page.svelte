8b14121cbdb354c6a14596f656b459f58b36a7ab
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-11-26 12:41:04 -0500

    resources

--- src/routes/[[preview=preview]]/resources/+page.svelte
+++ src/routes/[[preview=preview]]/resources/+page.svelte
@@ -15,31 +15,31 @@ import ContentWidth from "$lib/components/ContentWidth/ContentWidth.svelte";
     <ContentWidth class="border-t-2 border-dark flex flex-col md:flex-row py-10">
         <div class="md:w-1/2 pr-12 flex flex-col gap-10 items-start">
             <h3 class="text-dark">CHARGE SHEET</h3>
-            <BracketButton class="text-dark" href="/">DOWNLOAD</BracketButton>
+            <BracketButton class="text-dark" href="/pdfs/MSOT_ChargeSheet.pdf">DOWNLOAD</BracketButton>
         </div>
         <div class="mt-24 md:mt-0 md:w-1/2 pr-12 flex flex-col gap-10 items-start">
-            <p class="text-dark md:text-[24px] text-sm leading-normal">
+            <p class="text-dark md:text-[24px] text-sm leading-normal text-pretty break-words hyphens-manual w-full">
                 1583 Thousand Oaks Dr. <br/>
                 San Antonio, TX 78232 <br/><br/>
                 Phone: 210.744.9686 <br/>
                 Fax: 210.758.5466 <br/>
-                customerservice@MedicalSolutionsofTX.com
+                customerservice&shy;@MedicalSolutionsofTX.com
             </p>
             <BracketButton class="text-dark" href="/contact">Contact Us</BracketButton>
         </div>
 
     </ContentWidth>
 </section>
-<section class="bg-mid pt-24">
+<section class="bg-mid pt-24 pointer-events-none">
     <ContentWidth class="flex flex-col md:flex-row justify-between py-10">
-        <div class="md:w-[calc(33%-20px)] flex flex-col  mt-12 md:mt-0">
+        <div class="md:w-[calc(33%-20px)] flex flex-col  mt-12 md:mt-0 ">
             <h3 class="text-dark mb-14">Supplier 1</h3>
             <div class="text-dark w-full border-t-2 border-dark flex flex-row justify-between items-center py-5">
                 <a href="/" class="bump">
                     <h4 class="text-dark hover:opacity-90 transition-opacity">2024 Catalog</h4>
                 </a>
                 <a href="/" class="negative-bump">
-                    <i class="text-dark fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
+                    <i class="text-dark opacity-40 fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
                 </a>
             </div>
             <div class="text-dark w-full border-t-2 border-dark flex flex-row justify-between items-center py-5">
@@ -47,7 +47,7 @@ import ContentWidth from "$lib/components/ContentWidth/ContentWidth.svelte";
                     <h4 class="text-dark hover:opacity-90 transition-opacity">Technique Guide</h4>
                 </a>
                 <a href="/" class="negative-bump">
-                    <i class="text-dark fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
+                    <i class="text-dark opacity-40 fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
                 </a>
             </div>
             
@@ -59,7 +59,7 @@ import ContentWidth from "$lib/components/ContentWidth/ContentWidth.svelte";
                     <h4 class="text-dark hover:opacity-90 transition-opacity">2024 Catalog</h4>
                 </a>
                 <a href="/" class="negative-bump">
-                    <i class="text-dark fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
+                    <i class="text-dark opacity-40 fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
                 </a>
             </div>
             <div class="text-dark w-full border-t-2 border-dark flex flex-row justify-between items-center py-5">
@@ -67,7 +67,7 @@ import ContentWidth from "$lib/components/ContentWidth/ContentWidth.svelte";
                     <h4 class="text-dark hover:opacity-90 transition-opacity">Technique Guide</h4>
                 </a>
                 <a href="/" class="negative-bump">
-                    <i class="text-dark fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
+                    <i class="text-dark opacity-40 fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
                 </a>
             </div>
             
@@ -80,7 +80,7 @@ import ContentWidth from "$lib/components/ContentWidth/ContentWidth.svelte";
                     <h4 class="text-dark hover:opacity-90 transition-opacity">Technique Guide</h4>
                 </a>
                 <a href="/" class="negative-bump">
-                    <i class="text-dark fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
+                    <i class="text-dark opacity-40 fa-sharp fa-light fa-download fa-xl hover:opacity-90 transition-opacity"/>
                 </a>
             </div>
             
