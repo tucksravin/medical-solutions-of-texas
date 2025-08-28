f6267b81af966a5426726d5fcc5740ca66e55bf6
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-08-28 10:24:27 -0700

    dynamic dates

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -384,13 +384,13 @@ export let data:any;
 		<div class="flex flex-col justify-between items-start">
 			<a href="/" class="bump self-start h-16 md:h-20 relative"><img class="h-full transitionn duration-700 ease-fast-slow delay-500" src={msotLogo} style="transform-origin:top left" alt="msot logo"/></a>
 			{#if viewportWidth > 560}
-			<div class="label text-white ">©2024 - Medical Solutions of Texas  |   All Rights Reserved</div>
+			<div class="label text-white ">{"©" + new Date().getFullYear() + " - Medical Solutions of Texas  |   All Rights Reserved"}</div>
 			{/if}
 		
 		</div>
 		<img src={sdvosbLogo} alt="sdvosb logo" class="h-16 md:h-24"/>
 	</ContentWidth>
 	{#if viewportWidth <= 560}
-			<div class="label text-white w-full text-center px-2">©2024 - Medical Solutions of Texas  <br />   All Rights Reserved</div>
+			<div class="label text-white w-full text-center px-2">{"©" + new Date().getFullYear() + " - Medical Solutions of Texas"}  <br />   All Rights Reserved</div>
 	{/if}
 </footer>
